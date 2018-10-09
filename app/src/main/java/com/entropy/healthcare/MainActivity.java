7f785160bdef8127f7a60940c212fd5b2c977cf9
unknown <manjitborah2710@gmail.com>
2018-10-09 23:44:17 +0530

    First Commit

---
+++ app/src/main/java/com/entropy/healthcare/MainActivity.java
@@ -0,0 +1,32 @@
+package com.entropy.healthcare;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView welcomeText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        welcomeText=findViewById(R.id.welcome_text);
+        Animation animation=AnimationUtils.loadAnimation(getApplicationContext(),android.R.anim.fade_in);
+        animation.setDuration(5000);
+        welcomeText.setAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,LogInActivity.class));
+            }
+        },6000);
+
+    }
+}
